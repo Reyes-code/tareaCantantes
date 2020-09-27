179012c7689c13fe59777996ed7330e2579507ff
Santiago1707 <reyespiraligua29@gmail.com>
2020-09-26 23:50:54 -0500

    Tarea Musica - Carlos Reyes

---
+++ js/script.js
@@ -0,0 +1,77 @@
+function drake() {
+    let name= prompt("Ingrese el nombre de este artista");
+    let nombre= document.getElementById("nombre")
+
+    nombre.innerHTML= "<i>" + name + "</i>"
+
+
+    let gener = prompt("Genero musical")
+    let genero= document.getElementById("genero")
+
+    genero.innerHTML= "<i>" + gener + "</i>"
+
+
+    let pais= prompt("Pais de origen")
+    let country= document.getElementById("country")
+
+    country.innerHTML= "<i>" + pais + "</i>"
+
+
+    let cancion= prompt("Ingrese una canción de este artista")
+    let song= document.getElementById("song")
+
+    song.innerHTML= "<i>" + cancion + "</i>"
+
+}
+
+function jason() {
+    let name= prompt("Ingrese el nombre de este artista");
+    let nombre= document.getElementById("nombre2")
+
+    nombre2.innerHTML= "<i>" + name + "</i>"
+
+
+    let gener = prompt("Genero musical")
+    let genero= document.getElementById("genero2")
+
+    genero2.innerHTML= "<i>" + gener + "</i>"
+
+
+    let pais= prompt("Pais de origen")
+    let country= document.getElementById("country2")
+
+    country2.innerHTML= "<i>" + pais + "</i>"
+
+
+    let cancion= prompt("Ingrese una canción de este artista")
+    let song= document.getElementById("song2")
+
+    song2.innerHTML= "<i>" + cancion + "</i>"
+
+}
+
+function jBalvin() {
+    let name= prompt("Ingrese el nombre de este artista");
+    let nombre= document.getElementById("nombre3")
+
+    nombre3.innerHTML= "<i>" + name + "</i>"
+
+
+    let gener = prompt("Genero musical")
+    let genero= document.getElementById("genero3")
+
+    genero3.innerHTML= "<i>" + gener + "</i>"
+
+
+    let pais= prompt("Pais de origen")
+    let country= document.getElementById("country3")
+
+    country3.innerHTML= "<i>" + pais + "</i>"
+
+
+    let cancion= prompt("Ingrese una canción de este artista")
+    let song= document.getElementById("song3")
+
+    song3.innerHTML= "<i>" + cancion + "</i>"
+
+}
